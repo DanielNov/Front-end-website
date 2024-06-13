446d8e5efb739b484777819060dfb3a51a4258be
DanielNov <danielnovak378@gmail.com>
2024-06-13 23:50:28 +0200

    Start on the Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-slate-600 mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+
+            </div>
+        </nav>
+    );
+};
