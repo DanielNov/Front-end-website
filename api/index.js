4ade5ef15cd74abb1d8a347a351e2d98eb29f394
DanielNov <danielnovak378@gmail.com>
2024-08-30 16:15:09 +0200

    add register and login functionality

--- api/index.js
+++ api/index.js
@@ -1,1 +1,76 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+const jwt = require('jsonwebtoken');
+
+const prisma = new PrismaClient();
+
+app.use(cors({credentials: true, origin: "http://localhost:5173"}));
+app.use(express.json());
+
+const JWT_SECRET = "809e176390b96b8c38f517392b8b362cf39cd47a2ea67e0eb241881897dc9a7f";
+
+
+app.post('/register', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const existingUser = await prisma.user.findUnique({where: {email}});
+        if (existingUser) {
+            return res.status(400).json({error: 'User already exists'});
+        }
+
+        const hashedPass = await bcrypt.hash(password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                email,
+                password: hashedPass,
+            }
+        });
+
+        res.status(201).json({message: 'User registered successfully', userId: newUser.id});
+    }
+    catch (error) {
+        console.error('Error creating user', error);
+        res.status(500).json({error: 'Internal server error'});
+    }
+
+});
+
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({where: {email}});
+        if (!user) {
+            return res.status(400).json({error: 'Invalid email'});
+        }
+
+        const isPassOk = await bcrypt.compare(password, user.password);
+        if (!isPassOk) {
+            return res.status(400).json({error: 'Invalid password'});
+        }
+
+        const token = jwt.sign(
+            { userId: user.id, email: user.email},
+            JWT_SECRET,
+            {expiresIn: '1h'}
+        )
+
+        res.json({message: 'User logged in successfully', token});
+    }
+    catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({error: 'Internal server error'});
+    }
+})
+
+app.listen(3010);
+
+process.on('SIGINT', async () => {
+    await prisma.$disconnect();
+    process.exit();
+});
--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -1,6 +1,27 @@
 import PropTypes from "prop-types";
+import {useState} from "react";
 
 export default function LoginModal({ isOpen, onClose}) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function login (e){
+        e.preventDefault();
+        const response = await fetch("http://localhost:3010/login", {
+            method: "POST",
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
+        });
+
+        if (response.ok) {
+            onClose();
+        }
+        else {
+            alert("Wrong email or password");
+        }
+    }
+
     if (!isOpen) return null;
 
     return (
@@ -47,21 +68,25 @@ export default function LoginModal({ isOpen, onClose}) {
                                 <div className="h-px w-full bg-slate-200"></div>
                             </div>
 
-                            <form className="space-y-4 md:space-y-6" action="#">
+                            <form className="space-y-4 md:space-y-6" onSubmit={login}>
                                 <div>
                                     <label htmlFor="email"
                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                        email</label>
-                                    <input type="email" name="email" id="email"
+                                           email</label>
+                                    <input type="email"
                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                           placeholder="name@example.com" required=""/>
+                                           placeholder="name@example.com"
+                                           value={email}
+                                           onChange={(e) => setEmail(e.target.value)}/>
                                 </div>
                                 <div>
                                     <label htmlFor="password"
                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••"
+                                    <input type="password"
+                                           placeholder="••••••••"
                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                           required=""/>
+                                           value={password}
+                                           onChange={(e) => setPassword(e.target.value)}/>
                                 </div>
                                 <button type="submit"
                                         className="w-full text-black bg-white hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in
